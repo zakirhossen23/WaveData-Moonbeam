0875e0bf0332162cead08059143b3efe19726587
vybz.live <73053667+zakirhossen23@users.noreply.github.com>
2022-12-17 00:40:22 +0600

    fixed

--- wavedata-api/pages/api/GET/getFhir.js
+++ wavedata-api/pages/api/GET/getFhir.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   let useContract = await import("../../../contract/useContract.ts");
   let { contract, signerAddress } = await useContract.default();
-  let fhir_element = await contract._fhirMap(Number(req.query.userid).call());
+  let fhir_element = await contract._fhirMap(Number(req.query.userid)).call();
   
   var newFhir = {
     id: Number(fhir_element.user_id),
